e80029838d72637934ab00315f8c6b61cbfa7b5a
Clinton <manakanejustin@gmail.com>
2020-11-15 23:34:04 +0200

    removed the unnecessary console log when clicking nav menu

--- js/app.js
+++ js/app.js
@@ -3,6 +3,5 @@ const toggleNavbar = document.querySelector(".toggle-navbar");
 const navbarLinks = document.querySelector(".navbar-links");
 
 toggleNavbar.addEventListener("click", () => {
-  console.log(navbarLinks);
   navbarLinks.classList.toggle("toggle-navbar-on");
 });
